063d6d137be781ebdd75f9100000448a9931f8b4
pedroHenriq <pedropereira712@gmail.com>
2024-04-28 16:41:33 -0300

    Adicionando lista de token

--- main.c
+++ main.c
@@ -14,23 +14,38 @@ enum tokenType
 
 typedef struct token
 {
-    char *input;
     int type;
     int value;
+    char *input;
+    struct token *next;
 } token;
 
-int getTokenValue(char *input, char* tokenInput)
+typedef struct tokenList
+{
+    int count;
+    token *head;
+    token *tail;
+} tokenList;
+
+int getTokenValue(char **input, char **tokenInput)
 {
     int number = 0, counter = 0;
     do
     {
-        *tokenInput = *input;
-        number *= pow(10, counter);
-        number += (*input - '0');
-        input++; counter++; tokenInput++;
-    } while (isdigit(*input));
+        number *= 10;
+        number += ((**input) - '0');
+        (*input)++;
+        counter++;
+    } while (isdigit(**input));
 
-    *tokenInput = '\0';
+    (*input) -= counter;
+    *tokenInput = (char *)malloc(sizeof(char) * (counter + 1));
+
+    strncpy(*tokenInput, *input, counter);
+
+    *(*tokenInput + counter) = '\0';
+    (*input) += counter - 1;
+
     return number;
 }
 
@@ -58,32 +73,37 @@ int isOperator(char input)
     return 0;
 }
 
-int isMathFunction(char *input, char  *tokenInput)
+int isMathFunction(char **input, char **tokenInput)
 {
     int counter = 0;
     char **mathFunctions = (char *[]){"sin", "cos", "tan", "log10", "log", '\0'};
 
-    if (isalpha(*input))
+    if (isalpha(**input))
     {
         while (*mathFunctions)
         {
-            while (*input)
+            while (**input)
             {
-                if (**mathFunctions != *input)
+                if (**mathFunctions != **input)
                     break;
 
-                input++;
+                (*input)++;
                 (*mathFunctions)++;
                 counter++;
 
-                if (!(**mathFunctions)) {
+                if (!(**mathFunctions))
+                {
+                    (*input)--;
                     *mathFunctions -= counter;
-                    strcpy(tokenInput, *mathFunctions);
+
+                    *tokenInput = (char *)malloc(sizeof(char) * (counter + 1));
+                    strcpy(*tokenInput, *mathFunctions);
+
                     return 1;
                 }
             }
 
-            input -= counter;
+            (*input) -= counter;
             counter = 0;
             mathFunctions++;
         }
@@ -92,44 +112,100 @@ int isMathFunction(char *input, char  *tokenInput)
     return 0;
 }
 
-token getToken(char *input)
+token *getToken(char **input)
 {
-    token token;
+    token *token;
+    token = (struct token *)malloc(sizeof(struct token));
 
-    token.input = malloc(sizeof(char));
-    *token.input = *input;
-
-    token.value = *input;
-
-    if (isdigit(*input))
+    if (token == NULL)
     {
-        token.type = NUMBER;
-        token.value = getTokenValue(input, token.input);
+        printf("Memory's full!\n");
+        exit(0);
     }
-    else if (isOperator(*input))
-        token.type = OPERATOR;
-    else if (isMathFunction(input, token.input))
-        token.type = MATHFUNCTION;
-    else if (!isNormalizedToken(*input))
+
+    token->type = **input;
+    token->value = **input;
+
+    while (isspace(**input))
+        (*input)++;
+
+    if (isdigit(**input))
     {
-        token.input = "";
-        token.value = -1;
-        token.type = -1;
+        token->type = NUMBER;
+        token->value = getTokenValue(input, &token->input);
+    }
+    else if (isMathFunction(input, &token->input))
+        token->type = MATHFUNCTION;
+    else
+    {
+        token->input = (char *)malloc(sizeof(char) * 2);
+        *token->input = **input;
+        *(token->input + 1) = '\0';
+
+        if (isOperator(**input))
+            token->type = OPERATOR;
+        else if (!isNormalizedToken(**input))
+        {
+            token->value = -1;
+            token->type = -1;
+        }
     }
 
     return token;
 }
 
+void addToken(tokenList *tokenList, char **input)
+{
+    if (tokenList->head == NULL)
+    {
+        tokenList->head = getToken(input);
+        tokenList->tail = tokenList->head;
+    }
+    else
+    {
+        tokenList->tail->next = getToken(input);
+        tokenList->tail = tokenList->tail->next;
+    }
+
+    tokenList->tail->next = NULL;
+    tokenList->count++;
+}
+
+void showTokens(tokenList tokenList)
+{
+    token *token;
+    token = tokenList.head;
+    while (token != NULL)
+    {
+        printf("Token { input: %s, value: %d, type: %d }\n", token->input, token->value, token->type);
+        token = token->next;
+    }
+}
+
+tokenList getTokens(char *input)
+{
+    tokenList tokenList;
+    tokenList.head = NULL;
+
+    while (*input)
+    {
+        addToken(&tokenList, &input);
+        input++;
+    }
+
+    return tokenList;
+}
+
 int main()
 {
-    token token;
-    char cc[10];
+    char input[10];
+    tokenList tokenList;
 
     printf("f(x) = ");
-    scanf("%s", cc);
+    gets(input);
 
-    token = getToken(cc);
-    printf("Token: value: %s; type: %d;\n", token.input, token.type);
+    tokenList = getTokens(input);
+    showTokens(tokenList);
 
     return 0;
 }
