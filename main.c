5fe9c4dd6735c093526d12c6778cfdfb3359f7a2
Pedro <dev02@kapibara.com.br>
2024-05-23 17:58:01 -0300

    Concorrencia

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <signal.h>
 #include "compile.h"
 
 int **initMatrix(int xMax, int yMax)
@@ -45,9 +46,8 @@ void drawMatrix(int **matrix, int xSize, int ySize, int xSpacing, int ySpacing)
     printf("\n");
 }
 
-void showForXinRange(char *input, int xMax, int yMax, int xSpacing, int ySpacing)
+void showForXinRange(double *values, int xMax, int yMax, int xSpacing, int ySpacing)
 {
-    double *values;
     int **matrix;
     int y, x = 0;
     int xSize, ySize;
@@ -55,8 +55,6 @@ void showForXinRange(char *input, int xMax, int yMax, int xSpacing, int ySpacing
     ySize = (yMax / ySpacing) + 1;
     xSize = (xMax / xSpacing) + 1;
 
-    initTokenList(input, 1);
-    values = getForXinRange(0, xMax, xSpacing);
     matrix = initMatrix(xSize, yMax);
     do
     {
@@ -119,7 +117,7 @@ void configGraph(int *xMax, int *yMax, int *xSpacing, int *ySpacing)
     printf("***************************************\n\n");
 }
 
-void configPlane(int *speed, char *input)
+void configPlane(int *speed, char *input, int descriptor)
 {
     printf("\nAERONAVES\n");
     printf("***************************************\n");
@@ -131,25 +129,102 @@ void configPlane(int *speed, char *input)
     printf("f(x) = ");
     scanf("%s", input);
     printf("***************************************\n\n");
+
+    if (descriptor > 0)
+        kill(descriptor, SIGUSR1);
+    else 
+        signal(SIGUSR1, server);
 }
 
-void mapPlane(int xMax, int yMax, int xSpacing, int ySpacing)
+double *mapPlane(int descriptor, int xMax, int yMax, int xSpacing, int ySpacing)
 {
     char input[100];
     int speed;
 
-    configPlane(&speed, input);
-    showForXinRange(input, xMax, yMax, xSpacing, ySpacing);
+    configPlane(&speed, input, descriptor);
+    initTokenList(input, 1);
+    return getForXinRange(0, xMax, xSpacing);
+}
+
+char *getString(double *values, int xCount)
+{
+    char *buffer;
+    int xSize, bufferSize, x = 0;
+
+    xSize = xCount + 1;
+    bufferSize = sizeof(char) * 8 * xSize + 1;
+    buffer = malloc(bufferSize);
+    do
+    {
+        char number[9];
+        snprintf(number, 9, "%07d ", (int)*(values + x));
+        strcat(buffer, number);
+    } while (++x < xCount);
+
+    return buffer;
+}
+
+void server(int descriptor, int readfd, int writefd, int xMax, int yMax, int xSpacing, int ySpacing)
+{
+    char *buffer;
+    double *values;
+
+    values = mapPlane(descriptor, xMax, yMax, xSpacing, ySpacing);
+    buffer = getString(values, xMax / xSpacing);
+
+    printf("%s\n", buffer);
+    // showForXinRange(values, xMax, yMax, xSpacing, ySpacing);
 }
 
 int main()
 {
     int xMax, yMax;
     int ySpacing, xSpacing;
+    int pid;
+
+    int descriptor;
+    int planeSt[2], planeNd[2];
 
     printDescription();
     configGraph(&xMax, &yMax, &xSpacing, &ySpacing);
-    mapPlane(xMax, yMax, xSpacing, ySpacing);
+
+    pid = getpid();
+
+    if (pipe(planeSt) < 0 || pipe(planeNd) < 0)
+    {
+        printf("Error: cannot call pipe;\n");
+        exit(0);
+    }
+
+    if ((descriptor = fork()) < 0)
+    {
+        printf("Error: cannot call fork;\n");
+        exit(0);
+    }
+    else if (descriptor > 0)
+    {
+        close(planeSt[0]);
+        close(planeNd[1]);
+
+        server(descriptor, planeNd[0], planeSt[1], xMax, yMax, xSpacing, ySpacing);
+
+        close(planeSt[1]);
+        close(planeNd[0]);
+        exit(0);
+    }
+    else
+    {
+        close(planeSt[1]);
+        close(planeNd[0]);
+
+        signal(SIGUSR1, server);
+        pause();
+        server(descriptor, planeSt[0], planeNd[1], xMax, yMax, xSpacing, ySpacing);
+
+        close(planeSt[0]);
+        close(planeNd[1]);
+        exit(0);
+    }
 
     return 0;
 }
